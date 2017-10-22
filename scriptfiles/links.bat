28408095322025fe49917a2329896c87cfecb9a4
Sergei Marochkin <xziggix@gmail.com>
2017-10-22 18:04:27 +0300

    Add support for new fixes with DANGEROUS_SERVER_ROOT requires

---
+++ scriptfiles/links.bat
@@ -0,0 +1,4 @@
+rem Create a Windows soft link.  Parameters are in reverse to Linux.
+mklink /J DANGEROUS_SERVER_ROOT ..
+cd ..
+
---
+++ scriptfiles/links.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Try create the links.  Note, we will ONLY use lower-case directories.
+ln -s .. DANGEROUS_SERVER_ROOT
+cd ..
+
