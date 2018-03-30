ae92d98b01b97d31c78ae8d4b6841a250d5cb48d
Sergei Marochkin <xziggix@gmail.com>
2018-03-30 21:29:56 +0300

    Обновление скриптов сборки

--- !compile.bat
+++ !compile.bat
@@ -1,5 +1,3 @@
 set name=Open-GTO
-compiler\pawncc.exe -;+ -(+ -icompiler/includes sources\%name%.pwn
-if exist %name%.amx ^
-move %name%.amx gamemodes\
+compiler\pawncc.exe -;+ -(+ -icompiler/includes -isources -ogamemodes/%name%.amx sources/%name%.pwn
 pause
--- !compile.sh
+++ !compile.sh
@@ -5,11 +5,4 @@ export LD_LIBRARY_PATH=$(pwd)"/compiler/:$LD_LIBRARY_PATH"
 NAME="Open-GTO"
 PARAMS=$1
 
-./compiler/pawncc "-;+" "-(+" "-icompiler/includes" $PARAMS "sources/$NAME.pwn"
-
-if [ $(stat -c%s "$NAME.amx") -gt 0 ];
-then
-	mv $NAME.amx gamemodes/
-else
-	rm $NAME.amx
-fi
+./compiler/pawncc "-;+" "-(+" "-icompiler/includes" "-isources" $PARAMS "-ogamemodes/$NAME.amx" "sources/$NAME.pwn"
